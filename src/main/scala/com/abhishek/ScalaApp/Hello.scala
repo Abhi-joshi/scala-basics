a9afc3a1b7b6794a3178d6f4f70aef9172eeeaae
Abhishek Sharma <mail.abhishekjoshi@gmail.com>
2018-01-07 14:15:25 +0530

    Add more code for basic learning.

--- src/main/scala/com/abhishek/ScalaApp/Hello.scala
+++ src/main/scala/com/abhishek/ScalaApp/Hello.scala
@@ -1,9 +1,53 @@
 package com.abhishek.ScalaApp
 
-object Hello extends Greeting with App {
-  println(greeting)
+class Point(val xc: Int, val yc: Int) {
+  var x: Int = xc
+  var y: Int = yc
+
+  def move(dx: Int, dy: Int): Unit = {
+    x = x + dx
+    y = y + dy
+    println("Point x location : " + x);
+    println("Point y location : " + y);
+  }
+
+  var factor: Int = 3
+  val multiplier = (i: Int) => i * factor
+
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+class Demo {
+  var myArray = Array(1, 2, 3)
+  var total = 0
+  def printArray(): Unit = {
+    for (i <- myArray) {
+      println("Element " + i)
+    }
+  }
+  
+  def sumArray(): Int = {
+    for (i <- myArray) {
+      total += i
+    }
+    total
+  }
+  
+}
+
+object Hello {
+  val greeting: String = "hello"
+  def main(args: Array[String]): Unit = {
+    println(greeting)
+
+    val pt = new Point(10, 20)
+    // Move to a new location
+    pt.move(5, 9)
+
+    val mulValue: Int = pt.multiplier(8);
+    println("Result of multiplication is " + mulValue)
+    
+    val demo = new Demo();
+    demo.printArray();
+    println("Sum of array is " + demo.sumArray())
+  }
 }
--- src/test/scala/com/abhishek/ScalaApp/HelloSpec.scala
+++ src/test/scala/com/abhishek/ScalaApp/HelloSpec.scala
@@ -6,4 +6,20 @@ class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
   }
+  "The Point object" should "move to 15, 29" in {
+    val pt = new Point(10, 20)
+    pt.move(5, 9)
+    pt.x shouldEqual (15)
+    pt.y shouldEqual (29)
+  }
+  "The Multiplier closure" should "return 24" in {
+    val pt = new Point(10, 20)
+    val mulValue: Int = pt.multiplier(8)
+    mulValue shouldEqual (24)
+  }
+  "Sum of array" should "return 6" in {
+    val demo = new Demo()
+    val total = demo.sumArray();
+    total shouldEqual (6)
+  }
 }
